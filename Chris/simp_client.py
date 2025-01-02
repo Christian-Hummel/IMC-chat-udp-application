e67dfbe0b2dea75a7afce4a51693aa73a1cbadcd
Christian-Hummel <23imc10881@fh-krems.ac.at>
2025-01-02 23:14:10 +0100

    restructured client to be more compact

--- Chris/simp_client.py
+++ Chris/simp_client.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python3
-
 import socket
-import threading
 import sys
-import time
 import keyboard
 
+
 CLIENT_PORT = 7778
+
 
 
 def connect(ip_address, client_sock):
@@ -36,9 +34,11 @@ def receive(client_sock):
         pass
 
 
-def quit(host, message, client_sock):
-    client_sock.sendto(message, (host, CLIENT_PORT))
-    sys.exit()
+def quit(host, client_sock):
+    close = b'Exit'
+    client_sock.sendto(close, (host, CLIENT_PORT))
+
+
 
 
 def show_usage():
@@ -53,6 +53,7 @@ if __name__ == "__main__":
         # client also need to be bound to an ip and port
         client_sock.bind(("127.0.0.1", CLIENT_PORT))
 
+
         daemon_ip_request = sys.argv[1]
         daemon_ip = "" + daemon_ip_request
         print(f"daemon_ip_request {daemon_ip_request}")
@@ -60,66 +61,41 @@ if __name__ == "__main__":
         if connect(daemon_ip_request, client_sock):
             print(f"Successfully connected to daemon with ip {daemon_ip_request}")
 
-            username = input("Please enter your username: ")
-            send(username.encode(), daemon_ip, client_sock)
-
-            options = receive(client_sock)
-            print(options)
-
-            response = input()
-            send(response.encode(), daemon_ip, client_sock)
-
             while True:
 
                 data = receive(client_sock)
 
-                if data == "1":
-                    chat_request = input("Please enter the IP address of the user you want to chat with: ")
-                    send(chat_request.encode(), daemon_ip, client_sock)
-                    continue
+                if data == "Waiting for incoming chat requests, please wait or press q to exit":
+                    print(data)
 
-                elif data == "2":
-                    print("Waiting for incoming chat requests, press q disconnect")
+
+
                     while True:
-                        data = receive(client_sock)
 
-                        if data == "quit":
-                            print("Exiting conversation")
-                            client_sock.close()
+                        if keyboard.is_pressed("q"):
+                            break
 
-                        print(data)
-                        response = input("Enter your message: ")
-                        send(response.encode(), daemon_ip, client_sock)
+                        # This section is blocking if uncommented - keyboard input will not be
+                        # recognized
+                        #
+                        # data = receive(client_sock)
+                        #
+                        # print(data)
+                        #
+                        # response = input()
+                        #
+                        # send(response.encode("ascii"), daemon_ip, client_sock)
 
-                if data == "quit":
-                    print("Exiting conversation")
-                    client_sock.close()
+                    quit(daemon_ip, client_sock)
+                    break
+
+
 
                 print(data)
-                msg = input("Enter your message: ")
-                send(msg.encode(), daemon_ip, client_sock)
 
-                # chat functionality
-                # while True:
-                #
-                #
-                #     message = input("Enter your message")
-                #
-                #     # shut down client - daemon connection
-                #     if message == "!q":
-                #         quit(daemon_ip ,message.encode(), client_sock)
-                #
-                #     else:
-                #         send(daemon_ip, message.encode(), client_sock)
-                #         receive(client_sock)
+                response = input()
 
-    # while True:
-    #     client_input = input("1 for start conversation and 0 for wait")
-    #     if int(client_input) == 1:
-    #         data = send(sys.argv[1], str.encode(sys.argv[2]))
-    #         print('Received', data)
-    #
-    #     elif int(client_input) == 0:
-    #         receive(daemon_ip)
+                send(response.encode(), daemon_ip, client_sock)
+
 
 
